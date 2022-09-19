b428abba5dfb706eff7442aa09029edaa18ff291
k.maksimov <k.maksimov90@gmail.com>
2022-09-19 18:10:39 +0300

    Upload initial draft for EF Entities

---
+++ Models/Models/Post.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Post
+    {
+        public int PostID { get; set; }
+        public string PostTitle { get; set; }
+        public string PostContent { get; set; }
+        public int TagID { get; set; }
+        public int UserID { get; set; }
+        public int PostLikes { get; set; }
+        public List<Comment> Comments { get; set; }
+        public List<Tag> Tags { get; set; }
+
+    }
+}
---
+++ Models/Models/Tag.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Tag
+    {
+        public int TagID { get; set; }
+        public string TagName { get; set; }
+        public List<Post> Posts { get; set; }
+    }
+}
