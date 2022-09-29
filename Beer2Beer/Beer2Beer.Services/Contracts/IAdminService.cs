62829579c68f2034d22eeb63846b9f89750f80ad
k.maksimov <k.maksimov90@gmail.com>
2022-09-29 12:30:17 +0300

    Add Service Interfaces

---
+++ Beer2Beer/Beer2Beer.Services/Contracts/IAdminService.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using Beer2Beer.Data.Contracts;
+using Beer2Beer.DTO;
+using Beer2Beer.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beer2Beer.Services.Contracts
+{
+    public interface IAdminService
+    {
+        Task<UserDto> FindUserByUserName(string username);
+        Task<UserDto> FindUserByEmail(string email);
+        Task<List<UserDto>> FindUsersByFirstName(string firstName);
+        Task Promote(string username);
+        Task Demote(string username);
+        Task BlockUser(string username);
+        Task UnblockUser(string username);
+        Task IsUserNull(User user);
+
+    }
+}
---
+++ Beer2Beer/Beer2Beer.Services/Contracts/IUserService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Beer2Beer.Services.Contracts
+{
+    public interface IUserService
+    {
+    }
+}
