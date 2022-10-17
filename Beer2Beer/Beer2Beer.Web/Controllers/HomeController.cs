7880b6692aa2ee427151774e41038825f50440ef
k.maksimov <k.maksimov90@gmail.com>
2022-10-17 15:14:52 +0300

    Add HomeController again

---
+++ Beer2Beer/Beer2Beer.Web/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Beer2Beer.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
