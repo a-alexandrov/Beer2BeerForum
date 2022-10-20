3558d0be65bb5e62fd59276cc5cb1dfd25a10a53
Georgi Kostov <g3rg1.kostov@gmail.com>
2022-10-20 16:26:30 +0300

    Added routing and /post page. Cleaned up initial angular setup.

--- Beer2Beer/Beer2Beer.UI/src/app/app-routing.module.ts
+++ Beer2Beer/Beer2Beer.UI/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PostComponent } from './pages/post/post.component';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: AppComponent
+  },
+  {
+    path: 'post', component: PostComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Beer2Beer/Beer2Beer.UI/src/app/app.module.ts
+++ Beer2Beer/Beer2Beer.UI/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+import { PostComponent } from './pages/post/post.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    PostComponent,
   ],
   imports: [
     BrowserModule,
