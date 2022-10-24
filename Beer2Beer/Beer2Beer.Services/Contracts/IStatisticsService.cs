e867296b0187919f3611f1c69287607fe4f4bf1a
k.maksimov <k.maksimov90@gmail.com>
2022-10-25 01:10:41 +0300

    Add Statistics Service and Controller

---
+++ Beer2Beer/Beer2Beer.Services/Contracts/IStatisticsService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beer2Beer.Services.Contracts
+{
+    public interface IStatisticsService
+    {
+        Task<int> TotalUsersCount();
+        Task<int> TotalPostsCount();
+    }
+}
---
+++ Beer2Beer/Beer2Beer.Services/StatisticsService.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using Beer2Beer.Data.Contracts;
+using Beer2Beer.Models;
+using Beer2Beer.Services.Contracts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beer2Beer.Services
+{
+    public class StatisticsService : IStatisticsService
+    {
+        private readonly IBeer2BeerDbContext context;
+
+        public StatisticsService(IBeer2BeerDbContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<int> TotalPostsCount()
+        {
+            return await this.context.Set<Post>()
+                .Where(p=>!p.IsDeleted)
+                .CountAsync();
+        }
+
+        public async Task<int> TotalUsersCount()
+        {
+            return await this.context.Set<User>()
+                .Where(u => !u.IsDeleted)
+                .CountAsync();
+        }
+    }
+}
