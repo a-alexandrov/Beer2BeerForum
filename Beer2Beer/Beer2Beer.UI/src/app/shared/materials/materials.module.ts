adf42b095c6c33ac46b1f74d32a216a90bcb445a
Aleksandar Aleksandrov <aleksandar.aleksandrov.a41@learn.telerikacademy.com>
2022-10-25 17:10:53 +0300

    Added Button Module

--- Beer2Beer/Beer2Beer.UI/src/app/shared/materials/materials.module.ts
+++ Beer2Beer/Beer2Beer.UI/src/app/shared/materials/materials.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {MatButtonModule} from '@angular/material/button'
 
 
 
 @NgModule({
   declarations: [],
   exports: [
-    CommonModule
+    CommonModule,
+    MatButtonModule
   ]
 })
 export class MaterialsModule { }
