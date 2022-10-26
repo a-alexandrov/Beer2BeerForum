7168de3d98e209ee4e81cede6c6133953fb46a08
Georgi Kostov <g3rg1.kostov@gmail.com>
2022-10-26 08:27:19 +0300

    Refactoring, moved components in new folder

--- Beer2Beer/Beer2Beer.UI/src/app/app.module.ts
+++ Beer2Beer/Beer2Beer.UI/src/app/app.module.ts
@@ -1,28 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './pages/home/home.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 import { BirichkoComponent } from './shared/birichko/birichko.component';
 import { PostPageComponent } from './pages/post-page/post-page.component';
 import { PostComponent } from './pages/post-page/post/post.component';
-import { HeaderComponent } from './pages/header/header.component';
-import { CounterContainerComponent } from './shared/counter-container/counter-container.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { CounterContainerComponent } from './shared/components/counter-container/counter-container.component';
 import { ButtonContainerComponent } from './shared/button-container/button-container.component';
 import { HttpClientModule } from '@angular/common/http';
 import { CommentComponent } from './pages/post-page/comment/comment.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { UserPageComponent } from './pages/user-page/user-page.component';
-import { RateComponent } from './shared/rate/rate.component';
+import { RateComponent } from './shared/components/rate/rate.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import {MaterialsModule} from './shared/materials/materials.module'
+
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
+    HomePageComponent,
     PostComponent,
     PostPageComponent,
     BirichkoComponent,
