a8d312b308d99119a16094e353e724061176d28d
Georgi Kostov <g3rg1.kostov@gmail.com>
2022-10-28 16:22:18 +0300

    implement admin controls, change site icon, logo sign and background

--- Beer2Beer/Beer2Beer.UI/src/app/pages/admin-page/admin-controls/admin-controls.component.ts
+++ Beer2Beer/Beer2Beer.UI/src/app/pages/admin-page/admin-controls/admin-controls.component.ts
@@ -1,4 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, OnDestroy } from '@angular/core';
+import { Subject, takeUntil } from 'rxjs';
+import { AdminService } from 'src/app/core/services/admin.service';
 import { User } from 'src/app/shared/models/user.model';
 
 @Component({
@@ -6,14 +8,54 @@ import { User } from 'src/app/shared/models/user.model';
   templateUrl: './admin-controls.component.html',
   styleUrls: ['./admin-controls.component.css']
 })
-export class AdminControlsComponent implements OnInit {
+export class AdminControlsComponent implements OnInit, OnDestroy {
   @Input() user!: User;
-  constructor() { }
+  notifier = new Subject<void>;
+  constructor(private readonly adminService: AdminService) { }
 
   ngOnInit(): void {
   }
 
   onToggleUserBlocking() {
     this.user.isBlocked = !this.user.isBlocked;
+
+    if (this.user.isBlocked){
+      this.adminService.blockUserByUserName(this.user.username)
+        .pipe(takeUntil(this.notifier))
+        .subscribe((user) => {
+          this.user = user;
+        });
+    }
+    else {
+      this.adminService.unblockUserByUserName(this.user.username)
+        .pipe(takeUntil(this.notifier))
+        .subscribe((user) => {
+          this.user = user;
+        });
+    }
+  }
+
+  onToggleUserAdmin() {
+    this.user.isAdmin = !this.user.isAdmin;
+
+    if (this.user.isAdmin){
+      this.adminService.promoteUserByUserName(this.user.username)
+        .pipe(takeUntil(this.notifier))
+        .subscribe((user) => {
+          this.user = user;
+        });
+    }
+    else {
+      this.adminService.demoteUserByUserName(this.user.username)
+        .pipe(takeUntil(this.notifier))
+        .subscribe((user) => {
+          this.user = user;
+        });
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.notifier.next();
+    this.notifier.complete();
   }
 }
